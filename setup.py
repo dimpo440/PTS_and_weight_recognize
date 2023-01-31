d8964b4a495ba590ba13ea8e45913b2f90fd5373
dimpo440 <big.pog@gmail.com>
2023-01-31 03:20:13 +0200

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sts_and_weight',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='DP',
+    author_email='',
+    description=''
+)
